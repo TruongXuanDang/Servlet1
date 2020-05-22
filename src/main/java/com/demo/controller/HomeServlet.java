0cf355ae831f10c0cde1e8757bd18389333b479a
TruongDX8 <truonghusk17@gmail.com>
2020-05-22 19:23:11 +0700

    Life circle+Get param

---
+++ src/main/java/com/demo/controller/HomeServlet.java
@@ -0,0 +1,57 @@
+package com.demo.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HomeServlet")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        System.out.println("init");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+        System.out.println("service");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        System.out.println("destroy");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doPost");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doGet");
+        String name = request.getParameter("name");
+        String phone = request.getParameter("phone");
+        System.out.println(name);
+        PrintWriter out = response.getWriter();
+        out.println(name);
+        out.println(phone);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+        System.out.println("doPut");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+        System.out.println("doDelete");
+    }
+}
